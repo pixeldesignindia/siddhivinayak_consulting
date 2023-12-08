2103c2808a727a6c6e27d10b448471e6c4c9c69e
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-12-08 17:24:01 +0530

    done

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,9 +1,7 @@
-import client from "./sanity/client";
 import React from "react";
 import HomePage from "./components/homePage/HomePage";
-export default async function Home() {
-const pets = await client.fetch(`*[_type == "home"]`);
-console.log(pets)
+export default function Home() {
+
     return (
         <>
         <HomePage/>
