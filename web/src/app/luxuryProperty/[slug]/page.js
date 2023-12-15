0839144466853c2ef67ff5288e04c0751bac6907
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-12-15 10:13:43 +0530

    done

---
+++ web/src/app/luxuryProperty/[slug]/page.js
@@ -0,0 +1,82 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation'
+import { useEffect, useState , useRef } from 'react';
+import '../../aboutUs/about.css';
+import './property.css'
+import ClosiongNav from '../../components/ClosingNav/ClosiongNav';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import 'gsap/dist/gsap';
+import client from '../../sanity/client';
+gsap.registerPlugin(ScrollTrigger);
+import { PortableText } from "@portabletext/react";
+import Footer from '../../components/footer/Footer';
+function page(props) {
+
+  const containerRef = useRef(null);
+  const slug = props.params.slug
+
+  const [propertyData, setPropertyData] = useState(null);
+  useEffect(() => {
+    let scroll;
+    import("locomotive-scroll").then((locomotiveModule) => {
+        scroll = new locomotiveModule.default({
+            el: containerRef.current,
+            smooth: true,
+            direction: 'vertical',
+        });
+    });
+    return () => {
+        if (scroll) scroll.destroy();
+    }
+});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await client.fetch(`
+      *[slug.current == "${slug}"] {
+        title,
+        slug,
+        body,
+        mainImage {
+          asset -> {
+            _id,
+            url
+          },
+          alt,
+        },
+        "name":author -> name, 
+      } 
+    `);
+        console.log(data[0]);
+        setPropertyData(data[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [slug]);
+  return (
+    <div ref={containerRef}>
+      <ClosiongNav />
+      <div className='property-post-container'>
+        <div className='propertyImageTop' style={{
+                            backgroundImage: `url(${propertyData && propertyData.mainImage.asset.url})`
+                        }}></div>
+        <div className="property-content">
+          <h2 className="property-heading">
+          {propertyData && propertyData.title}
+          </h2>
+          <div className='property-Content-Box'>
+<PortableText value={propertyData && propertyData.body}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default page;
--- web/src/app/luxuryProperty/page.js
+++ web/src/app/luxuryProperty/page.js
@@ -11,6 +11,8 @@ import chart from '../../../public/images/chart.svg'
 import client from '../sanity/client';
 gsap.registerPlugin(ScrollTrigger);
 import Footer from '../components/footer/Footer';
+import Link from 'next/link';
+import './property.css'
 function page() {
 
     const containerRef = useRef(null);
@@ -32,6 +34,7 @@ function page() {
     });
 
     const [data, setData] = useState()
+    const [propertyCard, setPropertyCard] = useState()
     useEffect(() => {
         const newData = async () => {
             try {
@@ -51,40 +54,45 @@ function page() {
 
         newData();
     }, []);
-    // useEffect(() => {
-    //     const pin = gsap.to(sectionRef.current, {
-    //         y: 0,
-    //         scrollTrigger: {
-    //             trigger: sectionRef.current,
-    //             pin: true,
-    //             start: 'top top',
-    //             end: 'bottom bottom',
-    //             scrub: 1,
-    //         },
-    //     });
 
-    //     return () => {
-    //         pin.kill();
-    //     };
-    // }, []);
+    useEffect(() => {
+        client
+            .fetch(`
+            *[_type== "post"] {
+              title,
+              slug,
+              body,
+              mainImage {
+                asset -> {
+                  _id,
+                  url
+                },
+                alt,
+              },
+            }
+          `)
+            .then((data) => {
+                console.log(data)
+                setPropertyCard(data);
+            })
+            .catch(console.error);
+    }, []);
 
     return (
         <div ref={containerRef}>
             <ClosiongNav />
             <section class="gallery" data-scroll-section >
                 <div className="row posotion-container" id="pin">
-                    <div className="col-8 about-left-content" style={{ paddingTop: '10rem' }}>
+                    <div className="col-8 about-left-content" style={{ paddingTop: '10rem'}}>
                         <div className='title'>Luxury Properties</div>
                         <div className='heading top-heading'>{data && data.heading}</div>
-                        <div className='p-container'>
-
-                            {data && data.description && data.description.map((block, index) => (
-                                <p key={index}>{block.children[0].text} {block.children[1] && block.children[1].text} {block.children[2] && block.children[2].text}
-                                    {block.children[3] && block.children[3].text}
-                                    {block.children[4] && block.children[4].text}
-                                    {block.children[5] && block.children[5].text}
-                                </p>
-                            ))}
+                        <div className='p-container property-card-container' >
+                        {propertyCard && propertyCard.map((item, index) => (<Link href={`/luxuryProperty/${item.slug.current}`} key={index} >
+                            <div className='property-card'>
+                                <div className="image-property"><Image src={item.mainImage.asset.url} width={500} height={200} /></div>
+                                <h3>{item.title}</h3>
+                            </div>
+                        </Link>))}
                         </div>
                     </div>
                     <div className="col-4 about-img-c image-disappear">
@@ -93,15 +101,14 @@ function page() {
                         }}></div>
                     </div>
                     <div className="col-7 about-left-content">
-
                         {/* <div className='heading' style={{margin:'2rem 0'}}>Investors of Private Equity</div>
                     <Image src={chart} width={650}/>
                     <div className='heading' style={{margin:'2rem 0'}}>Private Equity PAN India</div>
                     <p>Looking for expert Private Equity assistance? Siddhi Vinayak Consulting is your trusted partner. We offer comprehensive support to businesses throughout their journey. From fundraising and mentorship to strategic advisory, we excel in every aspect. Our specialized team manages portfolio companies, identifies growth opportunities, and ensures optimal financial outcomes. Whether you’re a startup or an established firm, our tailored solutions cater to your unique needs. Experience the power of Private Equity in Mumbai, Pune, India with us, and take your business to new heights. Partner with Siddhi Vinayak Consulting for unparalleled expertise and success.</p> */}
                     </div>
                 </div>
-                <Footer/>
-                </section>
+                <Footer />
+            </section>
         </div>
     );
 }
